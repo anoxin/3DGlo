4dc8398c935ebfac2cb2e0efad32658a3ea2a3bc
Anton Anokhin <anoxin.toscha1@yandex.ru>
2022-04-23 13:52:05 +0300

    выполнение основного задания урока №20

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,17 +2,53 @@ const modal = () => {
     const modal = document.querySelector('.popup');
     const buttons = document.querySelectorAll('.popup-btn');
     const closeBtn = modal.querySelector('.popup-close');
+    let count = 0;
+    let close = false;
+    let idInterval;
     
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
+            close = false;
+            count = 0;
             modal.style.display = 'block';
+            if (screen.width > 768) {
+                animation();
+            }
+  
         });
     });
 
     closeBtn.addEventListener('click', () => {
-        modal.style.display = 'none';
+        close = true;
+        count = 1;
+        if (screen.width > 768) {
+            animation();
+        } else {
+            modal.style.display = 'none';
+        }
+        
     });
+
+      const animation = () => {
+        idInterval = requestAnimationFrame(animation);
+        if (close == false && count < 1) {
+            count = count + 0.05;
+            modal.style.opacity = count;
+        }
+        else if (close == true && count > 0) {
+            count = count - 0.05;
+            modal.style.opacity = count;
+        } else {
+            if (close == true && count < 0) {
+                modal.style.display = 'none';
+            }
+            cancelAnimationFrame(idInterval);
+        }
+
+    };
 
 };
 
 export default modal;
+
+console.dir(screen.width);
