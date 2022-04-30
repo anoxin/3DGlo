c0e89a77d47ea81716705367f17635f663f2f2a7
Anton Anokhin <anoxin.toscha1@yandex.ru>
2022-04-30 12:33:29 +0300

    выполнение основного задания урока №23

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal';
 import check from './modules/check';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
+import calc from './modules/calc';
 
 timer('3 may 2022');
 menu();
@@ -11,3 +12,4 @@ modal();
 check();
 tabs();
 slider();
+calc(100);
