47d12dc8433c0a3692058749684828b9aae44759
Anton Anokhin <anoxin.toscha1@yandex.ru>
2022-05-09 13:47:35 +0300

    Выполнение усложненного задания урока №21

--- src/modules/check.js
+++ src/modules/check.js
@@ -13,24 +13,27 @@ const check = () => {
     });
 
     inputsText.forEach(function(item) {
-        item.addEventListener('input', (e) => {
-            e.target.value = e.target.value.replace(/[^а-яёА-ЯЁ\s\-]/, "");
+        item.addEventListener('blur', (e) => {
+            e.target.value = e.target.value.replace(/[^а-яёА-ЯЁ\s\-]/g, "").trim() ;
+            e.target.value = e.target.value.substring(0, 1).toUpperCase() + e.target.value.substring(1).toLowerCase();
+            e.target.value = e.target.value.replace(/[\s]+/g, " ");
         });
     });
 
-    inputMessage.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^а-яёА-ЯЁ\s\-]/, "");
+    inputMessage.addEventListener('blur', (e) => {
+        e.target.value = e.target.value.replace(/[^а-яёА-ЯЁ\s\-]/g, "").trim();
+        e.target.value = e.target.value.replace(/[\s]+/g, " ");
     });
 
     inputsEmail.forEach(function(item) {
-        item.addEventListener('input', (e) => {
-            e.target.value = e.target.value.replace(/[^a-zA-Z0-9\@\-\_\.\!\~\*\']/, "");
+        item.addEventListener('blur', (e) => {
+            e.target.value = e.target.value.replace(/[^a-zA-Z0-9\@\-\_\.\!\~\*\']/g, "");
         });
     });
 
     inputsPhone.forEach(function(item) {
-        item.addEventListener('input', (e) => {
-            e.target.value = e.target.value.replace(/[^0-9\(\)\-\+]/, "");
+        item.addEventListener('blur', (e) => {
+            e.target.value = e.target.value.replace(/[^0-9\(\)\-\+]/g, "");
         });
 
     });
